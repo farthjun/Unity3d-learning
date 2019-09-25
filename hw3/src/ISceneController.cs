43a4e6cc408e41c06db9c954061ef00c69766e6c
王嘉浚 <13539062145@163.com>
2019-09-25 10:43:19 +0800

    upload to github

---
+++ hw3/src/ISceneController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace isc
+{
+    public interface ISceneController
+    {
+        void LoadResources();                   //加载场景
+    }
+}
+
---
+++ hw3/src/IUserAction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+using rolemodel;
+public interface IUserAction                           //用户互动会发生的事件
+{
+    void MoveBoat();                                   //移动船
+    void Restart();                                    //重新开始
+    void MoveRole(RoleModel role);                     //移动角色
+    int Check();                                       //检测游戏结束
+}
+
